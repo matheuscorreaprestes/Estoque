885159cbe7c391316f4fdc8e3e05598713ff35c3
Matheus Correa <matheus.correa22@edu.unifil.br>
2023-11-16 20:40:15 -0300

    SEGUNDA TENTATIVA

---
+++ scripts/login.js
@@ -0,0 +1,12 @@
+function Login() {
+    var done=0;
+    var usuario = document.getElementsByName('usuario')[0].value;
+    usuario=usuario.toLowerCase();
+    var senha= document.getElementsByName('senha')[0].value;
+    senha=senha.toLowerCase();
+    if (usuario=="admin" && senha=="admin") {
+      window.location="../pages/cadastro.html";
+      done=1;
+    }
+    if (done==0) { alert("Dados incorretos, tente novamente"); }
+  }
